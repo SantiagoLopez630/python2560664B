91b890377aede18d6c6abce39b1ac85f12be9e58
Santiago Lopez <dev.santiagolopez@gmail.com>
2023-04-09 19:46:16 -0500

    First commit

--- Python intermedio/sqlite3/Proyecto Atlas/dbpy2.py
+++
@@ -1,7 +0,0 @@
-def modificar(conexion,tabla,campo,dato,id):
-    micursor=conexion.cursor()
-    sentencia=f"UPDATE {tabla} SET {campo}='{dato}' WHERE id='{id}'"
-    micursor.execute(sentencia)
-    con.commit()
-    print('Modificación exitosa')
-
